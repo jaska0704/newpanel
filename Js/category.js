0671501cb365239c8a7d50600294b80e32820c67
Jasurbek <jaskasamadov695@gmail.com>
2023-12-08 10:23:08 +0500

    manhtfdddssdsdfasgffdfdfdvdfs

---
+++ Js/category.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded", async () => {
+    let form = document.querySelector("form");
+    let tbody = document.querySelector("tbody");
+
+    form.addEventListener("submit", async (e) => {
+        e.preventDefault();
+
+        let uz = form[0].value; 
+        let ru = form[1].value; 
+        let image = form[2].value;
+        
+        let {data} = await axios.post("http://localhost:5050/api/v1/categories", {
+            uz,
+            ru,
+            image,
+        }, {
+            headers: {
+                authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+        });
+
+        form.reset();
+        window.location.reload();
+    });
+
+        let { data } = await axios.post(
+          "http://localhost:5050/api/v1/categories",
+          {
+            headers: {
+              authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
+        );
+
+        data.forEach(element => {
+            let tr = document.createElement("tr");
+            let ru = document.createElement("td");
+            let uz = document.createElement("td");
+
+            let image = document.createElement("img");
+            image.setAttribute("src", catagory.image);
+
+            ru.textContent = catagory.ru;
+            uz.textContent = catagory.uz;
+
+            image.style.width = "150px";
+            image.style.height = "150px";
+
+            tr.append(uz, ru, image);
+
+            tbody.append(tr); 
+        });
+
+         logout.addEventListener("click", (e) => {
+           e.preventDefault();
+
+           localStorage.removeItem("user-token");
+           window.location.replace("login.html");
+         });
+
+    
+});
--- Js/products.js
+++ Js/products.js
@@ -2,50 +2,100 @@ document.addEventListener("DOMContentLoaded", async () => {
   let form = document.querySelector("#form1");
   let button = document.querySelector(".button1");
   let tbody = document.querySelector("tbody");
+  let select = document.querySelector("select");
 
   button.addEventListener("click", async (e) => {
     e.preventDefault();
-    let name = form[0].value;
-    let meta = form[1].value;
-    let price = form[2].value;
-    let color = form[3].value;
-    let description = form[4].value;
-    let image = form[5].value;
-    let category = form[6].value;
 
-    let data = await axios.post(
-      "http://localhost:5050/api/v1/products",
-      {
-        name,
-        meta,
-        price,
-        color,
-        description,
-        image,
-        category,
-      });
+    nameUz = form[0].value;
+    nameRu = form[1].value;
+    price = form[2].value;
+    colorUz = form[3].value;
+    colorRu = form[4].value;
+    descriptionUz = form[5].value;
+    descriptionRu = form[6].value;
+    image = form[7].value;
+    category = form[8].value;
+
+    let newProduct = {
+      name: {
+        uz: nameUz,
+        ru: nameRu,
+      },
+      color: {
+        uz: colorUz,
+        ru: colorRu,
+      },
+      price,
+      description: {
+        uz: descriptionUz,
+        ru: descriptionRu,
+      },
+      image,
+      category,
+    };
 
     form.reset();
     window.location.reload();
-  });
-  let datae = await axios.get("http://localhost:5050/api/v1/products", {
+    let {data:products} = await axios.post(
+      "http://localhost:5050/api/v1/products",
+      newProduct,
+      {
+        headers: {
+          authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }
+    );
   });
 
-  datae.data.forEach((element, index) => {
-    let tr = document.createElement("tr");
-    tr.innerHTML = `
+  let { data: categories } = await axios.get(
+    "http://localhost:5050/api/v1/categories",
+    {
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    }
+  );
+  categories.forEach((element) => {
+    let option = document.createElement("option");
+
+    option.innerText = element.uz;
+    option.setAttribute("value", element._id);
+    select.append(option);
+  });
+
+  let { data: products } = await axios.get(
+    "http://localhost:5050/api/v1/products",
+    {
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    }
+  );
+
+   products.forEach((element, index) => {
+     let tr = document.createElement("tr");
+     tr.innerHTML = `
              <th scope="row">${index + 1}</th>
-                <td>${element.name}</td>
-                <td>${element.meta}</td>
+                <td>
+                uz:${element.name.uz}<br>ru:${element.name.ru}
+                </td>
                 <td>${element.price}</td>
-                <td>${element.color}</td>
-                <td>${element.description}</td>
-                <td><img  class="rounded-circle object-fit-cover" width="50" height="50" src=${
+                <td>${element.color.uz}</td>
+                <td>${element.description.uz}</td>
+                <td><img  class="rounded-circle object-fit-cover" width="50" height="50" src="${
                   element.image
-                }/></td>
-                <td>${element.category}</td>
+                }"/></td>
+                <td>${element.category?.uz}</td>
                 <td><buttontype="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@fat">Creat <i class="fa-solid fa-pen fa-flip"></i></buttontype=></td>
           `;
-    tbody.append(tr);
-  });
+     tbody.append(tr);
+   });
+
+   logout.addEventListener("click", (e) => {
+     e.preventDefault();
+
+     localStorage.removeItem("token");
+     window.location.replace("userlogin.html");
+   });
 });
